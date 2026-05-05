3a4eaf4bd4655b838f33c4e92c3cf27c1a375dcf
Taniya1493 <taniyapushey@gmail.com>
2026-05-05 23:30:35 +0530

    read api for accounts microservice

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -3,6 +3,7 @@ package com.eazybytes.accounts.controller;
 import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,15 @@ public class AccountsController {
         accountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201,AccountsConstants.MESSAGE_201));
+
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                                                               @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                               String mobileNumber){
+        CustomerDto customerDto = accountsService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
 
     }
 }
